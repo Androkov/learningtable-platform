7a42a3bdd85f235a79e0a8022ece13348e2eec5b
Androkov <leoffv.quattro@gmail.com>
2026-01-16 17:48:55 -0300

    Update App.jsx

--- learning-table/src/App.jsx
+++ learning-table/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from '@/context/ThemeContext';
-import { ContentProvider } from '@/context/ContentContext';
-import HomePage from '@/pages/HomePage';
-import ReadingPage from '@/pages/ReadingPage';
-import { Toaster } from '@/components/ui/toaster';
+import { ThemeProvider } from './context/ThemeContext';
+import { ContentProvider } from './context/ContentContext';
+import HomePage from './pages/HomePage';
+import ReadingPage from './pages/ReadingPage';
+import { Toaster } from './components/ui/toaster';
 
 function App() {
   return (
